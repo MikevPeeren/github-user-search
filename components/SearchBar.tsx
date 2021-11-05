4928aabd7c71d950bd98e4bbef8c5a750b597bc9
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-05 12:20:38 +0100

    :recycle: adjustments to searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -11,22 +11,22 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="rounded-xl flex flex-row justify-between items-center h-[69px] bg-lm-white dark:bg-dm-blue shadow-xl">
-      <div className="mx-8 flex items-center">
-        <div className="w-[24px] h-[25px]">
+    <div className="flex flex-row justify-between items-center h-[69px] bg-lm-white dark:bg-dm-blue shadow-xl rounded-xl overflow-hidden">
+      <div className="flex items-center ml-4">
+        <div className="w-6 h-6">
           <Image src={SEARCH_ICON} layout="responsive" alt="Search Icon" />
         </div>
         <input
-          className="mx-6 bg-transparent dark:text-dm-white text-lg font-normal placeholder-lm-blue dark:placeholder-dm-white md:w-80 outline-none hover:cursor-pointer"
+          className="md:pl-4 pl-4 bg-transparent dark:text-dm-white md:text-lg text-xs font-normal placeholder-lm-blue dark:placeholder-dm-white outline-none hover:cursor-pointer md:w-[300px] sm:w-[200px] w-[175px]"
           type="text"
           ref={textInput}
-          placeholder="Search Github Username..."
+          placeholder="Search Github username..."
         />
       </div>
       <div>
-        <span className="px-2 text-error hidden">No results</span>
+        <span className="md:ml-2 ml-1 text-error hidden">No results</span>
         <button
-          className="rounded-xl mx-2 text-lm-white dark:text-dm-white bg-dm-light-blue py-3 px-5 outline-none hover:cursor-pointer hover:bg-dm-light-blue-hover"
+          className="rounded-xl mx-2 text-lm-white dark:text-dm-white font-bold bg-dm-light-blue py-3 px-5 outline-none hover:cursor-pointer hover:bg-dm-light-blue-hover"
           type="submit"
           onClick={handleSubmit}
         >
