90b0e20720afd48a355ecb128183c2acf78d72f9
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-07 19:51:32 +0100

    :iphone: adjustments to responsiveness

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -11,19 +11,29 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="flex flex-row justify-between items-center h-[69px] bg-lm-white dark:bg-dm-blue shadow-xl rounded-xl overflow-hidden">
-      <div className="flex items-center md:ml-4 ml-1">
+    <div className="flex items-center h-[69px] bg-lm-white dark:bg-dm-blue shadow-xl rounded-xl overflow-hidden">
+      <div className="flex flex-row justify-between flex-grow items-center md:mx-4 mx-2">
         <div className="md:w-6 w-4 md:h-6 h-4">
           <Image src={SEARCH_ICON} layout="responsive" alt="Search Icon" />
         </div>
         <input
-          className="md:pl-4 pl-1 bg-transparent dark:text-dm-white md:text-lg text-xs font-normal placeholder-lm-blue dark:placeholder-dm-white outline-none hover:cursor-pointer overflow-ellipsis"
+          className="flex-grow md:pl-4 pl-1 bg-transparent dark:text-dm-white md:text-lg text-xs font-normal placeholder-lm-blue dark:placeholder-dm-white outline-none hover:cursor-pointer overflow-ellipsis"
           type="text"
           ref={textInput}
           placeholder="Search Github username..."
         />
+        <span className="md:mx-2 mx-1 text-error md:text-lg text-xs hidden">
+          No results
+        </span>
+        <button
+          className="rounded-xl text-lm-white dark:text-dm-white font-bold bg-dm-light-blue md:mx-2 mx-1 md:py-3 py-2 md:px-5 px-2 outline-none hover:cursor-pointer hover:bg-dm-light-blue-hover md:text-lg text-xs"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Search
+        </button>
       </div>
-      <div>
+      {/* <div>
         <span className="md:ml-2 ml-1 text-error hidden">No results</span>
         <button
           className="rounded-xl text-lm-white dark:text-dm-white font-bold bg-dm-light-blue md:mx-2 mr-1 md:py-3 py-2 md:px-5 px-2 outline-none hover:cursor-pointer hover:bg-dm-light-blue-hover"
@@ -32,7 +42,7 @@ const SearchBar = () => {
         >
           Search
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
