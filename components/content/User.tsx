e59c80c5d810ae8502fd727a62e90659a8b10501
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-07 20:02:05 +0100

    :recycle: adjusted joined text

--- components/content/User.tsx
+++ components/content/User.tsx
@@ -12,12 +12,16 @@ const User = () => {
         height={117}
         alt="Picture of the author"
       />
-      <div className="flex flex-col justify-around mx-10 px-2">
-        <h1 className="text-lm-dark-grey dark:text-dm-white">The Octocat</h1>
-        <h3 className="text-lm-light-blue dark:text-dm-light-blue py-1">
-          @octocat
+      <div className="flex lg:flex-row flex-col justify-around lg:justify-between items-baseline mx-10 px-2">
+        <div className="flex flex-col">
+          <h1 className="text-lm-dark-grey dark:text-dm-white">The Octocat</h1>
+          <h3 className="text-lm-light-blue dark:text-dm-light-blue py-1">
+            @octocat
+          </h3>
+        </div>
+        <h3 className="text-lm-grey dark:text-dm-white lg:ml-32">
+          Joined 25 Jan 2011
         </h3>
-        <h3 className="text-lm-grey dark:text-dm-white">Joined 25 Jan 2011</h3>
       </div>
     </div>
   );
