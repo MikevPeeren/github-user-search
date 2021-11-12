2a578f786bfdfb040b04d4bfd140374b0001cb45
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-12 12:53:45 +0100

    :recycle: changing back to h2

--- components/content/User.tsx
+++ components/content/User.tsx
@@ -27,9 +27,9 @@ const User = ({ name, login, joined, avatarUrl }: IUser) => {
             <h1 className="text-lm-dark-grey dark:text-dm-white">
               {name ? name : "The Octocat"}
             </h1>
-            <p className="text-lm-light-blue dark:text-dm-light-blue py-1 aTag">
+            <h2 className="text-lm-light-blue dark:text-dm-light-blue py-1">
               @{login ? login : "octocat"}
-            </p>
+            </h2>
           </div>
           <h3 className="text-lm-grey dark:text-dm-white lg:ml-32">
             Joined{" "}
